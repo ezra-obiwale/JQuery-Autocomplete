7a9715ef290096da1377d0996114d8cec8ddd6c0
Ezra Obiwale <contact@ezraobiwale.com>
2016-05-10 21:11:36 +0100

    Created library. Ready to go.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # JQuery-Autocomplete
 JQuery function to autocomplete a form input field from an array of data objects which can be set in the javascript or gotten via a url
+
+# Usage
+````js
+$('input').autocomplete();
+
+or
+
+````html
+<input type="input" data-jq-autocomplete />
+
+# Settings
+
+Key|Default|Description
+---|-------|-----------
+url|empty string|Path from which to access the data
+ul_class|empty string|The class to add to the ul of the displayed list
+data|empty array|Array of data to autocomplete from
+min_chars|3|Minimum characters before autocomplete kicks in
+delay|300|Time in millisecond before autocomplete kicks in
+method|get|Request method for ajax request. Url must be set
+data_key|empty string|Key in the response object containing the data array
+sift_data|false|Set to `true` if to check data against input string
+beforeListing|function|Function to call before data is loaded
+list|function|Function to load each received data
+afterListing|function|function to call after data is loaded
+
+# Demo
+Check the contained demo.html
---
+++ jquery-autocomplete.js
@@ -0,0 +1,105 @@
+(function () {
+	$.strToObj = function (str) {
+		var obj = {},
+				arr = str.replace(/[\{\}]/g, '').split(',');
+		$.each(arr, function () {
+			var _arr = this.split(':');
+			obj[_arr[0].replace(/[\"\']/g, '').trim()] = _arr[1].replace(/[\"\']/g, '').trim();
+		});
+		return obj;
+	};
+	$.checkAutocompleteData = function (input, data) {
+		var isAllowed = false;
+		$.each(data, function () {
+			if (this.toLowerCase().indexOf($(input).val().toLowerCase()) !== -1) {
+				isAllowed = true;
+				return false;
+			}
+		});
+		return isAllowed;
+	};
+	$.loadAutocompleteData = function (input, ul, config, data) {
+		var before = $(input).data('beforeListing') ?
+				window[$(input).data('beforeListing')] :
+				config.beforeListing,
+				list = $(input).data('list') ?
+				window[$(input).data('list')] :
+				config.list
+				, after = $(input).data('afterListing') ?
+				window[$(input).data('afterListing')] :
+				config.afterListing;
+		if (typeof before === 'function')
+			before.call($(input), $(ul), data);
+		var check = $(input).data('siftData') || config.sift_data || false;
+		if (!data) {
+			data = config.data;
+			check = true;
+		}
+		$.each(data, function () {
+			if (check && !$.checkAutocompleteData(input, this))
+				return;
+			if (typeof list === 'function')
+				list.call($(input), $(ul).append('<li />').children().last(), this);
+		});
+		if (typeof after === 'function')
+			after.call($(input), $(ul), data);
+	};
+	$.fn.autocomplete = function (settings) {
+		var config = {
+			url: '',
+			ul_class: '',
+			data: [],
+			min_chars: 3,
+			delay: 300,
+			method: 'get',
+			data_key: '',
+			sift_data: false,
+			beforeListing: function (ul, resp) {
+				$(ul).html('');
+			},
+			list: function (li, data) {
+				$.each(data, function () {
+					$(li).append('<p>' + this + '</p>');
+				});
+			},
+			afterListing: function (ul, resp) {
+
+			}
+		};
+		$.extend(config, settings);
+		var to = [];
+		this.each(function (i) {
+			var _config = {};
+			$.extend(_config, config);
+			if ($(this).data('jqAutocomplete')) {
+				var data = $(this).data('jqAutocomplete');
+				$.extend(_config, typeof data === 'object' ? data : $.strToObj($(this).data('jqAutocomplete')));
+			}
+			to.push(undefined);
+			$(this).data('aci', i).on('keyup', function () {
+				clearTimeout(to[$(this).data('aci')]);
+				to[$(this).data('aci')] = setTimeout(function () {
+					var timeout = $(this).data('minChars') || _config.min_chars || 3;
+					if (!$(this).val() || $(this).val().length < timeout) {
+						$(this).siblings('ul.completing-list').html('');
+						return;
+					}
+					var $input = $(this),
+							$ul = $input.siblings('ul.completing-list');
+					if (_config.data.length) {
+						$.loadAutocompleteData($input, $ul, _config);
+					} else if (_config.url.length || $input.data('url')) {
+						$[$input.data('method') || _config.method]($input.data('url') || _config.url, {query: $(this).val().trim()}, function (resp) {
+							var dataKey = $input.data('dataKey') || _config.data_key;
+							$.loadAutocompleteData($input, $ul, _config, dataKey ? resp[dataKey] : resp);
+						}, 'json');
+					}
+				}.bind(this), _config.delay);
+			}).wrap('<div/>').parent().addClass('auto-completing').append('<ul class="completing-list ' + ($(this).data('ulClass') || _config.ul_class) + '"/>');
+		});
+		return this;
+	};
+	$(function () {
+		$('[data-jq-autocomplete]').autocomplete();
+	});
+})(jQuery);
